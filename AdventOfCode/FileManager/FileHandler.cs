7415ea19533d88995a23dadfd748e4e91289e8e1
Timo Schaal <timo.schaal@werbas.com>
2024-09-24 12:19:08 +0200

    Addet FileManager

---
+++ AdventOfCode/FileManager/FileHandler.cs
@@ -0,0 +1,55 @@
+﻿namespace FileManager;
+
+public class FileHandler
+{
+    private static string _basePath = @"C:\Users\tschaal\RiderProjects\AdventOfCode\AdventOfCode\Saves";
+    
+    static FileHandler()
+    {
+        if (!Directory.Exists(_basePath))
+        {
+            Directory.CreateDirectory(_basePath);
+        }
+    }
+    
+    public static void CreateFile(string projectName, string className, string content)
+    {
+        string projectPath = Path.Combine(_basePath, projectName);
+        
+        if (!Directory.Exists(projectPath))
+        {
+            Directory.CreateDirectory(projectPath);
+        }
+        
+        string filePath = Path.Combine(projectPath, $"{className}.txt");
+        
+        File.WriteAllText(filePath, content);
+
+        Console.WriteLine($"File created at: {filePath}");
+    }
+    
+    public static string ReadFile(string projectName, string className)
+    {
+        string filePath = Path.Combine(_basePath, projectName, $"{className}.txt");
+
+        if (!File.Exists(filePath))
+        {
+            Console.WriteLine("File not found!");
+            return null;
+        }
+        return File.ReadAllText(filePath);
+    }
+    
+    public static void AppendToFile(string projectName, string className, string content)
+    {
+        string filePath = Path.Combine(_basePath, projectName, $"{className}.txt");
+
+        if (!File.Exists(filePath))
+        {
+            Console.WriteLine("File not found!");
+            return;
+        }
+        File.AppendAllText(filePath, content);
+        Console.WriteLine("Content appended to file.");
+    }
+}
---
+++ AdventOfCode/FileManager/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace FileManager;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        FileHandler.CreateFile("Advent2015", "Day1", "null");
+    }
+}
